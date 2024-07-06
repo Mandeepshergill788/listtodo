36aa8a44215d5f36264b62464e6ff8f9dd7eb0e1
Mandeep Kaur <156492360+Mandeepshergill788@users.noreply.github.com>
2024-07-05 21:24:48 -0400

    androidproject

---
+++ TodoList/app/src/main/java/com/trios2024mks/todolist/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.trios2024mks.todolist
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.trios2024mks.todolist.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var todoAdapter: TodoAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+        todoAdapter = TodoAdapter(mutableListOf())
+        binding.rvTodoItems.adapter = todoAdapter
+        binding.rvTodoItems.layoutManager = LinearLayoutManager(this)
+
+        binding.btnAddTodo.setOnClickListener {
+            val todoTitle = binding.etTodoTitle.text.toString()
+            if (todoTitle.isNotEmpty()) {
+                val todo = Todo(todoTitle)
+                todoAdapter.addTodo(todo)
+                binding.etTodoTitle.text.clear()
+            }
+        }
+
+        binding.btnDeleteDoneTodos.setOnClickListener {
+            todoAdapter.deleteDoneTodos()
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ TodoList/app/src/main/java/com/trios2024mks/todolist/Todo.kt
@@ -0,0 +1,6 @@
+package com.trios2024mks.todolist
+
+class Todo(
+    val title: String,
+    var isChecked: Boolean = false
+)
